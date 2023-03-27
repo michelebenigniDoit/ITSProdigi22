ca4ef8de64cb9aa3b8de62d11473fa4724ec9c82
Michele.Benigni <michelebenigni.mb@gmail.com>
2023-03-27 12:35:11 +0200

    Aggiungere i file di progetto.

---
+++ PrimaApp/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.BackgroundColor = ConsoleColor.DarkBlue;
+        Console.Clear();
+        string nome;
+        int slm = int.Parse("1");
+        Console.WriteLine("Come ti chiami?");
+
+        nome = Console.ReadLine();
+
+        Console.WriteLine("Ciao "+nome+"!");
+        
+    }
+}
